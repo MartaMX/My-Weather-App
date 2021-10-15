d34012cc1f1bbf61ccba5445beeb6d4fa44cb0f8
MartaMX <92443035+MartaMX@users.noreply.github.com>
2021-10-15 16:34:56 +0200

    JS API integration

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-//alert("Hello!");
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+}
+
+let apiKey = "23ab03a6498fafc28975a1bf7ad1e307";
+let cityStart = "Amsterdam";
+let units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityStart}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(displayTemperature);
