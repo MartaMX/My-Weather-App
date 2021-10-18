da0f0f2ee1782ec0bb2a08acb0118506a26195f2
MartaMX <92443035+MartaMX@users.noreply.github.com>
2021-10-18 17:00:39 +0200

    JS Automated Description, day, time, humidity and wind

--- src/app.js
+++ src/app.js
@@ -4,7 +4,36 @@ function displayTemperature(response) {
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
+
+let now = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let dayElement = document.querySelector("#day");
+dayElement.innerHTML = `${day}, `;
+let timeElement = document.querySelector("#time");
+timeElement.innerHTML = `${hours}:${minutes}`;
 
 let apiKey = "23ab03a6498fafc28975a1bf7ad1e307";
 let cityStart = "Amsterdam";
