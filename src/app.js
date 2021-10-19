c692813369816f60c78ba76fc501dea742026a4b
MartaMX <92443035+MartaMX@users.noreply.github.com>
2021-10-19 13:05:14 +0200

    JS-HTML Added search engine

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+//Time and date
+
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let dayElement = document.querySelector("#day");
+let timeElement = document.querySelector("#time");
+dayElement.innerHTML = `${day}, `;
+timeElement.innerHTML = `${hours}:${minutes}`;
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -18,32 +41,23 @@ function displayTemperature(response) {
   iconTodayElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let now = new Date();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let hours = now.getHours();
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
+function search(city) {
+  let apiKey = "23ab03a6498fafc28975a1bf7ad1e307";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayTemperature);
+  // alert("here");
 }
 
-let dayElement = document.querySelector("#day");
-dayElement.innerHTML = `${day}, `;
-let timeElement = document.querySelector("#time");
-timeElement.innerHTML = `${hours}:${minutes}`;
+function HandleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  //  console.log(cityInputElement.value);
+  //  alert(`${cityInputElement.value}`);
+}
 
-let apiKey = "23ab03a6498fafc28975a1bf7ad1e307";
-let cityStart = "Buenos Aires";
-let units = "metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityStart}&appid=${apiKey}&units=${units}`;
-
-axios.get(apiUrl).then(displayTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", HandleSubmit);
+let cityStart = "New York";
+search(cityStart);
